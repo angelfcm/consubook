9544b3033358d47180ac1bed2184b5c623b07646
angelfcm <angelfcm1@gmail.com>
2015-04-15 16:04:28 -0500

    Comportamiento de mostrar filtro de búsqueda al hacer foco en la caja de búsqueda y otros cambios no llamativos

--- public/js/consubook.js
+++ public/js/consubook.js
@@ -15,23 +15,26 @@ ngConsubook
 // Comportamiento para mostrar el dropdown de filtro de búsqueda al hacer focus en el input de búsqueda
 $(function(){
 
-	var $dropdown = $('#dropdown-search-by');
-	var $dropdown_menu = $dropdown.find('.dropdown-menu');
-	// Error inesperado cuando se utiliza dropdown('toggle') el comportamiento de los botones deja de funcioanr, no es posible seleccionar despues de esto
-/*
+	var $dropdown = $('#dropdown-search-by .dropdown-toggle');
+	var $dropdown_menu = $dropdown.next('.dropdown-menu');
+	var close = true;
+	var scope  = $('#consubook-btn-search')[0];
+
  	$('#consubook-btn-search').on('focus', function(){
 
- 		
- 		if ( $dropdown_menu.css('display') == 'none' ) {
- 			$dropdown.on("hide.bs.dropdown", function() { return false; });
- 			$dropdown_menu.dropdown('toggle');
- 		}
+ 		//if ( $dropdown.attr('aria-expanded') == 'false' ) {
+ 			$dropdown.parent().off('hide.bs.dropdown').on('hide.bs.dropdown', function() {  return false; });
+ 			$dropdown.dropdown('toggle');
+ 			this.focus();
+ 		//}
  	}).on('blur', function(){
- 		
- 		if ( $dropdown_menu.css('display') == 'block' ) {
- 			$dropdown.off("hide.bs.dropdown");
- 			$dropdown_menu.dropdown('toggle');
- 		}
+
+ 		//if ( $dropdown.attr('aria-expanded') == 'true' ) {
+ 		$dropdown_menu.one('click', function(){  
+			scope.focus();
+ 		});
+ 		//}	
+ 		$dropdown.parent().off("hide.bs.dropdown");
  	});
-*/
+
 });
