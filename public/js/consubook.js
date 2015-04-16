429e287c61635fd1530df62d352084771bf9966a
angelfcm <angelfcm1@gmail.com>
2015-04-15 21:46:22 -0500

    Solución a bug encontrado en el comportamiento de la caja de búsqueda y filtros. Además se integró logotipo consubook

--- public/js/consubook.js
+++ public/js/consubook.js
@@ -8,10 +8,6 @@ ngConsubook
 
 }]);
 
-
-
-
-
 // Comportamiento para mostrar el dropdown de filtro de búsqueda al hacer focus en el input de búsqueda
 $(function(){
 
@@ -19,22 +15,21 @@ $(function(){
 	var $dropdown_menu = $dropdown.next('.dropdown-menu');
 	var close = true;
 	var scope  = $('#consubook-btn-search')[0];
+	var focused = false;
+	var blured = false;
 
- 	$('#consubook-btn-search').on('focus', function(){
+ 	$dropdown_menu.find('.btn').on('click', function(){ 
 
- 		//if ( $dropdown.attr('aria-expanded') == 'false' ) {
- 			$dropdown.parent().off('hide.bs.dropdown').on('hide.bs.dropdown', function() {  return false; });
- 			$dropdown.dropdown('toggle');
- 			this.focus();
- 		//}
- 	}).on('blur', function(){
-
- 		//if ( $dropdown.attr('aria-expanded') == 'true' ) {
- 		$dropdown_menu.one('click', function(){  
-			scope.focus();
- 		});
- 		//}	
- 		$dropdown.parent().off("hide.bs.dropdown");
+ 		$dropdown.parent().one('hide.bs.dropdown', function() { return false; });
+ 		scope.focus();
  	});
 
-});
+ 	$('#consubook-btn-search').on('focus', function(){
+		
+ 		$dropdown.dropdown('toggle');
+ 		$dropdown.parent().one('hide.bs.dropdown', function() {  return false; });
+
+ 		this.focus();
+ 	});
+
+}); 
